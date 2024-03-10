dca3e24e95e5e2ba993340372d0ecb3bb654d150
tsimp010 <tsimp010@fiu.edu>
2024-03-10 17:23:05 -0400

    Keep These changes

--- lab-insta-parse/Post/PostViewController.swift
+++ lab-insta-parse/Post/PostViewController.swift
@@ -52,6 +52,37 @@ class PostViewController: UIViewController {
         present(picker, animated: true)
 
     }
+    
+    
+    @IBAction func onTakePhotoTapped(_ sender: Any) {
+        // TODO: Pt 2 - Present camera
+        // Make sure the user's camera is available
+        // NOTE: Camera only available on physical iOS device, not available on simulator.
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("❌📷 Camera not available")
+            return
+        }
+
+        // Instantiate the image picker
+        let imagePicker = UIImagePickerController()
+
+        // Shows the camera (vs the photo library)
+        imagePicker.sourceType = .camera
+
+        // Allows user to edit image within image picker flow (i.e. crop, etc.)
+        // If you don't want to allow editing, you can leave out this line as the default value of `allowsEditing` is false
+        imagePicker.allowsEditing = true
+
+        // The image picker (camera in this case) will return captured photos via it's delegate method to it's assigned delegate.
+        // Delegate assignee must conform and implement both `UIImagePickerControllerDelegate` and `UINavigationControllerDelegate`
+        imagePicker.delegate = self
+
+        // Present the image picker (camera)
+        present(imagePicker, animated: true)
+        
+
+
+    }
 
     @IBAction func onPickedImageTapped(_ sender: UIBarButtonItem) {
         // TODO: Pt 1 - Present Image picker
@@ -179,4 +210,23 @@ extension PostViewController: PHPickerViewControllerDelegate {
     }
     
 
+}
+extension PostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Dismiss the image picker
+            picker.dismiss(animated: true)
+
+            // Get the edited image from the info dictionary (if `allowsEditing = true` for image picker config).
+            // Alternatively, to get the original image, use the `.originalImage` InfoKey instead.
+            guard let image = info[.editedImage] as? UIImage else {
+                print("❌📷 Unable to get image")
+                return
+            }
+
+            // Set image on preview image view
+            previewImageView.image = image
+
+            // Set image to use when saving post
+            pickedImage = image
+    }
 }
